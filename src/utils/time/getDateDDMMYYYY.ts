c788b4d098d29b48887ff529443b1eb53a748090
gaskeo <zeytunyan2004@mail.ru>
2023-05-09 18:38:10 +0500

    feat(date): improve util for humanize dates

--- src/utils/time/getDateDDMMYYYY.ts
+++ src/utils/time/getDateDDMMYYYY.ts
@@ -1,46 +1,22 @@
-function getDateDDMMYYYY(date: Date, needYear = true) {
-    let dateRow = `${date.getDate()} `
-    switch (date.getMonth()) {
-        case 0:
-            dateRow += "января";
-            break;
-        case 1:
-            dateRow += "февраля";
-            break;
-        case 2:
-            dateRow += "марта";
-            break;
-        case 3:
-            dateRow += "апреля";
-            break;
-        case 4:
-            dateRow += "мая";
-            break;
-        case 5:
-            dateRow += "июня";
-            break;
-        case 6:
-            dateRow += "июля";
-            break;
-        case 7:
-            dateRow += "августа";
-            break;
-        case 8:
-            dateRow += "сентября";
-            break;
-        case 9:
-            dateRow += "октября";
-            break;
-        case 10:
-            dateRow += "ноября";
-            break;
-        case 11:
-            dateRow += "декабря";
-            break;
-    }
-    if (!needYear) return dateRow;
+const months = [
+    "января",
+    "февраля",
+    "марта",
+    "апреля",
+    "мая",
+    "июня",
+    "июля",
+    "августа",
+    "сентября",
+    "октября",
+    "ноября",
+    "декабря"
+]
 
-    return `${dateRow} ${date.getFullYear()}`;
+function getDateDDMMYYYY(date: Date, needYear = true) {
+    if (!needYear) return `${date.getDate()} ${months[date.getMonth()]}`;
+
+    return `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
 }
 
 export {getDateDDMMYYYY};
